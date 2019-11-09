c734483e531a9158a9db381fd116a021885b3e40
PriyanshuNandan <31548996+priyanshuN@users.noreply.github.com>
2019-11-09 20:04:49 +0530

    update1

--- portal/php/update_check.php
+++ portal/php/update_check.php
@@ -14,11 +14,16 @@ if($_GET['action']=="check"){
     $sql1="select * from project where Name = '$uname'";
     $result = mysqli_query($conn,$sql);
     if(mysqli_num_rows($result)){
-        echo "<table class='table table-striped'><thead><tr><th scope='col'>Paper_ID</th><th scope='col'>Name</th><th scope='col'>Type</th><th scope='col'>YearofPub</th></tr></thead><tbody>";
+        echo "<p><br>Click and edit the fields of above record you want to update";
+        echo "<div class='card'><h5 class='card-header text-center font-weight-bold text-uppercase py-4'>Results</h5><div class='card-body'><table class='table table-striped' id='rectable'><thead><tr><th scope='col'>Paper_ID</th><th scope='col'>Name</th><th scope='col'>Type</th><th scope='col'>YearofPub</th></tr></thead><tbody>";
         while($row=mysqli_fetch_assoc($result)){
-            echo "<tr><th scope='row'>{$row['Paper_ID']}</th><td>{$row['Name']}</td><td>{$row['Type']}<td>{$row['YearofPub']}";
+            echo "<tr><th scope='row'>{$row['Paper_ID']}</th><td contenteditable='true'>{$row['Name']}</td><td contenteditable='true'>{$row['Type']}<td contenteditable='true'>{$row['YearofPub']}";
         }
-        echo "</tbody></table>";
+        echo "</tbody></table></div></div>";
+       
+    }
+    else{
+        echo"<div class='alert alert-danger' role='alert' id='rAlert' >No record found!</div>";
     }
     
     exit();
