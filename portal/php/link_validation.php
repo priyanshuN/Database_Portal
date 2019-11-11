a2687f54d455136cc04b9cd7d9482e1478d1596f
PriyanshuNandan <31548996+priyanshuN@users.noreply.github.com>
2019-11-12 01:11:54 +0530

    link

--- portal/php/link_validation.php
+++ portal/php/link_validation.php
@@ -3,18 +3,37 @@
 	$dbuser="root";
 	$dbpass="mysql";
     $dbname="database_project_cs355";
-    echo "hi";
+    
     
 	$conn= mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
     $conn1= new mysqli($dbhost,$dbuser,$dbpass,$dbname);
 	if(!$conn){
 		die("Connection failed: \n");
 	}
-    $email='iitp_project@rediff.com';
-    $hash=md5(rand(0,1000));
-    $to=$email;
-    $subject='Link validation';
-    $message="PLease click the link http://localhost/Database_Portal/portal/php/linkredirect.php?email='.$email.'$hash='.$hash.'";
-    $headers='From"iitp_project@rediff.com'."\r\n";
-    mail($to,$subject,$message,$headers);
+    if(isset($_POST['submit'])){
+        $email=$_POST['email'];
+        $hash=md5(rand(0,1000));
+        $sql="insert into link_check(email,hash,active) values('$email','$hash',0)";
+        echo $sql;
+        $result=mysqli_query($conn,$sql);
+        if($result){
+            $to=$email;
+            $subject='Link validation check';
+            $message="PLease click the link http://localhost/Database_Portal/portal/php/linkredirect.php?email='.$email.'&$hash='.$hash.'";
+            $headers='From:"abc@rediff.com'."\r\n";
+            mail($to,$subject,$message,$headers);
+        }
+        else{
+            echo"query";
+        }
+    }
+    else{
+        echo"fail1";
+    }
 ?>
+
+<fieldset>
+    <form action="" method="post">
+        Email:<input type="email" name='email'>
+        <input  type='submit' name='submit' value='submit'>
+</fieldset>
--- portal/php/linkredirect.php
+++ portal/php/linkredirect.php
@@ -3,7 +3,7 @@
 	$dbuser="root";
 	$dbpass="mysql";
     $dbname="database_project_cs355";
-    echo "hi";
+    
     
 	$conn= mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
     $conn1= new mysqli($dbhost,$dbuser,$dbpass,$dbname);
@@ -14,10 +14,11 @@
     if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
         $email = mysql_escape_string($_GET['email']); // Set email variable
         $hash = mysql_escape_string($_GET['hash']); // Set hash variable
-        $sql="select * from link_check where email='$email' and hash='$hash' and active='0'";
+        $sql="select * from link_check where email=$email and hash=$hash and active=0";
         $result=mysqli_connect($conn,$sql);
         if(mysqli_num_rows($result)>0){
-            $sql="update link_check set active='1' where email='$email' and hash='$hash'";
+            $sql="update link_check set active=1 where email='$email' and hash='$hash'";
+            $result=mysqli_connect($conn,$sql);
             echo "<div class='statusmsg'>Your link verified</div>";
         }
         else{
