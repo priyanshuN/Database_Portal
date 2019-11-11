d9aa50323b5411d37d55c39744cf9f77dc7c5b28
PriyanshuNandan <31548996+priyanshuN@users.noreply.github.com>
2019-11-11 22:27:21 +0530

    queries

--- portal/php/insert_rec.php
+++ portal/php/insert_rec.php
@@ -33,54 +33,69 @@
             echo $error;
             exit();
         }
-        $id="17";
-        $email="mak";
-        if($type==1)
-        $sql="insert into publication values('$id','$name','publication','$year')";
-        if($type==2)
-        $sql="insert into publication values('$id','$name','journal','$year')";
-        if($type==3)
-        $sql="insert into project values('$id','$name',$budget,'$year')";
-        $sql3="insert into project_own values('$id','$email','YES')";
-        if($ncol>0)
-        {
-            for($i=1;$i<=$ncol;$i++)
-            {
-                $temp=$names[$i-1];
-                $sql2="insert into project_own values('$id','$temp','NO')";
-                $result2 = mysqli_query($conn,$sql2);
-                if(($result2 )>0){
-                    echo  "success";
-                }
-                else{
-                $error = "Data entered is not correct";
-                }
-            }
+//        $id="171";
+//        $email="mak";
+//        if($type==1)
+//        $sql="insert into publication values('$id','$name','publication','$year')";
+//        if($type==2)
+//        $sql="insert into publication values('$id','$name','journal','$year')";
+//        if($type==3)
+//        $sql="insert into project values('$id','$name',$budget,'$year')";
+//        $sql3="insert into project_own values('$id','$email','YES')";
+        for($i=1;$i<=$ncol;$i++){
+            $to=$names[$i-1];
+            echo $to;
+            $subject='Submit confirmation';
+            $message=" is being entered if its yours please click on the click.  ";
+            $headers='From:abc@gmail.com'."\r\n";
+            mail($to,$subject,$message,$headers);
+            
         }
-        echo $sql,$sql2,$sql3;
-        $result = mysqli_query($conn,$sql);
-        $result3 = mysqli_query($conn,$sql3);
-        if(($result )>0){
-                echo  "success";
-        }
-        else{
-            $error = "Data entered is not correct1";
-        }
-        if(($result3 )>0){
-            echo  "success";
-    }
-        else{
-            $error = "Data entered is not correct2";
-        }
-        
-
-        if($error!=""){
-            echo $error;
-            exit();
-        }
-        else{
-            //echo "success";
-            exit();
-        }
+//        if($ncol>0)
+//        {
+//            for($i=1;$i<=$ncol;$i++)
+//            {
+//                $temp=$names[$i-1];
+//                $sql2="insert into project_own values('$id','$temp','NO')";
+//                $result2 = mysqli_query($conn,$sql2);
+//                if(($result2 )>0){
+//                    echo  "success";
+//                    $to=$names[i-1];
+//                    echo $to;
+//                    $subject='Submit confirmation';
+//                    $message="'$name' is being entered if its yours please click on the click.  ";
+//                    $headers='From:abc@gmail.com'."\r\n";
+//                    mail($to,$subject,$message,$headers);
+//                }
+//                else{
+//                $error = "Data entered is not correct";
+//                }
+//            }
+//        }
+//        echo $sql,$sql2,$sql3;
+//        $result = mysqli_query($conn,$sql);
+//        $result3 = mysqli_query($conn,$sql3);
+//        if(($result )>0){
+//                echo  "success";
+//        }
+//        else{
+//            $error = "Data entered is not correct1";
+//        }
+//        if(($result3 )>0){
+//            echo  "success";
+//        }
+//        else{
+//            $error = "Data entered is not correct2";
+//        }
+//        
+//        
+//        if($error!=""){
+//            echo $error;
+//            exit();
+//        }
+//        else{
+//            //echo "success";
+//            exit();
+//        }
     }
 ?>
