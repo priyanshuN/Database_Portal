91b40cf5d0510ce7644473ae5cced96669b42697
PriyanshuNandan <31548996+priyanshuN@users.noreply.github.com>
2019-11-13 01:56:14 +0530

    update

--- portal/php/update_check.php
+++ portal/php/update_check.php
@@ -9,18 +9,33 @@
 		die("Connection failed: \n");
 	}
 if($_GET['action']=="check"){
-    $uname=$_POST['ubname'];
-    $sql="select * from publication where Name ='$uname'";
-    $sql1="select * from project where Name = '$uname'";
-    $result = mysqli_query($conn,$sql);
-    if(mysqli_num_rows($result)){
-        echo "<p><br>Click and edit the fields of above record you want to update";
-        echo "<div class='card'><h5 class='card-header text-center font-weight-bold text-uppercase py-4'>Results</h5><div class='card-body'><table class='table table-striped' id='rectable'><thead><tr><th scope='col'>Paper_ID</th><th scope='col'>Name</th><th scope='col'>Type</th><th scope='col'>YearofPub</th></tr></thead><tbody>";
-        while($row=mysqli_fetch_assoc($result)){
-            echo "<tr><th scope='row'>{$row['Paper_ID']}</th><td contenteditable='true'>{$row['Name']}</td><td contenteditable='true'>{$row['Type']}<td contenteditable='true'>{$row['YearofPub']}";
+    $uname=mysqli_real_escape_string($conn,$_POST['ubname']);
+    $type=mysqli_real_escape_string($conn,$_POST['type']);
+    if($type=='Project'){
+        $sql1="select * from project where Name = '$uname'";
+        $result1 = mysqli_query($conn,$sql1);
+        if(mysqli_num_rows($result1)){
+            echo "<p><br>Click and edit the fields of above record you want to update";
+            echo "<div class='card'><h5 class='card-header text-center font-weight-bold text-uppercase py-4'>Results</h5><div class='card-body'><table class='table table-striped' id='rectable'><thead><tr><th scope='col'>Paper_ID</th><th scope='col'>Name</th><th scope='col'>Budget</th><th scope='col'>YearofPro</th></tr></thead><tbody>";
+            while($row=mysqli_fetch_assoc($result1)){
+                echo "<tr><th scope='row'>{$row['Paper_ID']}</th><td contenteditable='true'>{$row['Name']}</td><td contenteditable='true'>{$row['Budget']}<td contenteditable='true'>{$row['YearofPro']}";
+            }
+            echo "</tbody></table></div></div>";
+
         }
-        echo "</tbody></table></div></div>";
-       
+    }
+    elseif($type=='Paper' || $type=='Journal'){
+        $sql="select * from publication where Name ='$uname'";
+        $result = mysqli_query($conn,$sql);
+        if(mysqli_num_rows($result)){
+            echo "<p><br>Click and edit the fields of above record you want to update";
+            echo "<div class='card'><h5 class='card-header text-center font-weight-bold text-uppercase py-4'>Results</h5><div class='card-body'><table class='table table-striped' id='rectable'><thead><tr><th scope='col'>Paper_ID</th><th scope='col'>Name</th><th scope='col'>Type</th><th scope='col'>YearofPub</th></tr></thead><tbody>";
+            while($row=mysqli_fetch_assoc($result)){
+                echo "<tr><th scope='row'>{$row['Paper_ID']}</th><td contenteditable='true'>{$row['Name']}</td><td contenteditable='true'>{$row['Type']}<td contenteditable='true'>{$row['YearofPub']}";
+            }
+            echo "</tbody></table></div></div>";
+
+        }
     }
     else{
         echo"<div class='alert alert-danger' role='alert' id='rAlert' >No record found!</div>";
---
+++ portal/php/update_final.php
@@ -0,0 +1,48 @@
+<?php
+    $dbhost="localhost";
+	$dbuser="root";
+	$dbpass="mysql";
+    $dbname="database_project_cs355";
+    $flag=0;
+    
+	$conn= mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+    $conn1= new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+	if(!$conn){
+		die("Connection failed: \n");
+	}
+    if($_GET['action']=='update'){
+        $type=mysqli_real_escape_string($conn,$_POST['type1']);
+        $id=mysqli_real_escape_string($conn,$_POST['id']);
+        //echo $type;
+        if($type=='Project'){
+            $name=mysqli_real_escape_string($conn,$_POST['name']);
+            $budget=mysqli_real_escape_string($conn,$_POST['budget']);
+            $year=mysqli_real_escape_string($conn,$_POST['year']);
+            $sql="update project set Name='$name' ,Budget='$budget',YearofPro='$year' where Paper_ID='$id'";
+            //echo $sql;
+            $result=mysqli_query($conn,$sql);
+            if($result){
+                echo "Updated successfully";
+            }
+            else{
+                echo "Updation failed";
+            }
+        }
+        
+        if($type=='Paper'||$type=='Journal'){
+            $name=mysqli_real_escape_string($conn,$_POST['name']);
+            $year=mysqli_real_escape_string($conn,$_POST['year']);
+            $sql="update publication set Name='$name' ,YearofPub='$year' where Paper_ID='$id'";
+            $result=mysqli_query($conn,$sql);
+            if($result){
+                echo "Updated successfully";
+            }
+            else{
+                echo "Updation failed";
+            }
+        }
+    }
+    else{
+        echo"error";
+    }
+?>
