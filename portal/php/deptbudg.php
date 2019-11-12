6cc07071f1e37dd7965a5bc3f77a36d181c51275
PriyanshuNandan <31548996+priyanshuN@users.noreply.github.com>
2019-11-12 23:47:52 +0530

    trend

--- portal/php/deptbudg.php
+++ portal/php/deptbudg.php
@@ -11,5 +11,27 @@
 		die("Connection failed: \n");
 	}
     if($_GET['action']=='budget'){
+        $dept=mysqli_real_escape_string($conn,$_POST['dept']);
+        $year=mysqli_real_escape_string($conn,$_POST['year']);
+        if($dept!='0' && $year!=""){
+            $sql="select Department,sum(Budget) as Total_Budget from  professor,project,project_own where project.Paper_ID=project_own.paper_ID and professor.User_ID=project_own.User_ID and Department='$dept' and YearofPro='$year'";
+            $result=mysqli_query($conn,$sql);
+            if(mysqli_num_rows($result)>0){
+                $row=mysqli_fetch_assoc($result);
+                echo "<div class='card'><h5 class='card-header text-center font-weight-bold text-uppercase py-4'>Results</h5><div class='card-body'><table class='table table-striped' id='yearbudpro'><thead><tr><th scope='col'>Department</th><th scope='col'>Total_Budget</th></tr></thead><tbody>";
+                echo "<tr><th scope='row'>{$row['Department']}</th><td >{$row['Total_Budget']}</td></tr>";
+                echo "</tbody></table></div></div>";
+            }
+            else{
+                echo "No records";
+            }
+        }
+        else{
+            echo "Fill correctly";
+        }
     }
+    else{
+        echo "error";
+    }
+    exit();
 ?>
